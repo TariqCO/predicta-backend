f7706a2d5af504108a23652a284b9826908b16b8
Tariq Rasheed <tariq.official1712@gmail.com>
2026-08-25 18:00:09 +0500

    changes in index.js file

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 
 // ──────────── Middleware ────────────
 const corsOptions = {
-  origin: "*",
+  origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
